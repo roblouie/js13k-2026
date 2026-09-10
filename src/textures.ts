44e93405d07992780c977261dc8c728f10d1c239
Rob <rob.j.louie@gmail.com>
2026-09-09 20:29:22 -0400

    shrinking, 1 byte too big

--- src/textures.ts
+++ src/textures.ts
@@ -56,7 +56,7 @@ export async function initTextures() {
   textureLoader.load_(await solidColor('#333'));
   textureLoader.load_(await solidColor('pink') );
   textureLoader.load_(await solidColor('#fff'));
-  textureLoader.load_(await solidColor('#902EBB'));
+  textureLoader.load_(await solidColor('#902ebb'));
 
   // NOTE: In the depth fragment shader the texture depth is checked to determine shadows, so that these don't cast shadows. 🌸
   for (let i = 0; i < 8; i++) {
